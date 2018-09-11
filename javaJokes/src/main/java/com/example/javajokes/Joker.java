d9ab0ce8ac43b17f98eb16a90215ab785f5aad89
Divya Ambadipudi <divya.shastry#gmail.com>
2018-09-10 20:39:44 -0700

    Closes #5: Added asynctask to pull jokes from GCE and GCE pulls jokes from java library

--- javaJokes/src/main/java/com/example/javajokes/Joker.java
+++ javaJokes/src/main/java/com/example/javajokes/Joker.java
@@ -2,6 +2,8 @@ package com.example.javajokes;
 
 public class Joker {
     public String getJoke() {
-        return "This is my first funny joke :) Now laugh ...";
+        return "This is my first funny joke coming from " +
+                "Java library to GCE to the android app to the android library activity" +
+                " to be in front of your eyes :)";
     }
 }
